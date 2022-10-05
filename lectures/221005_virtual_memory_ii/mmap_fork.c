17edc259b1187005371e12d4b17c34a594de2fef
Troels Henriksen <athas@sigkill.dk>
2022-10-05 08:58:50 +0200

    More code and a Makefile.

--- lectures/221005_virtual_memory_ii/mmap_fork.c
+++ lectures/221005_virtual_memory_ii/mmap_fork.c
@@ -7,31 +7,36 @@
 #include <sys/mman.h>
 #include <assert.h>
 
-int main(int argc, char** argv) {
-  char *filename = argv[1];
-  int num_bytes = 10000;
+size_t file_size(FILE* f) {
+  assert(fseek(f, 0, SEEK_END) == 0);
+  int end = ftell(f);
+  assert(fseek(f, 0, SEEK_SET) == 0);
+  return end;
+}
 
-  FILE* file = fopen(filename, "r");
-  assert(file != NULL);
-  fseek(file, 0, SEEK_END);
-  int size = ftell(file);
-  fseek(file, 0, SEEK_SET);
+int main(int argc, char** argv) {
+  assert(argc == 2);
+  char *filename = argv[1];
+
+  FILE* f = fopen(filename, "r");
+  assert(f != NULL);
+  size_t size = file_size(f);
 
   unsigned char *data = mmap(NULL,
                              size,
                              PROT_WRITE | PROT_READ,
-                             MAP_ANON | MAP_SHARED,
+                             MAP_ANONYMOUS | MAP_SHARED,
                              0, 0);
   assert(data != MAP_FAILED);
 
   pid_t pid = fork();
   if (pid == 0) {
-    assert(fread(data, 1, size, file) == size);
+    assert(fread(data, 1, size, f) == size);
     exit(0);
   } else {
-    fclose(file);
+    fclose(f);
     waitpid(pid, NULL, 0);
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
       putchar(data[i]);
     }
   }
