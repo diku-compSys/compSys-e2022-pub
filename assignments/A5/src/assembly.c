9a95cacdbc99ff2b627e1845a247d2e36622c66c
Finn Schiermer Andersen <finn.schiermer.andersen@gmail.com>
2022-12-08 10:31:59 +0100

    reimplemented hashing in A5

--- assignments/A5/src/assembly.c
+++ assignments/A5/src/assembly.c
@@ -1,4 +1,5 @@
 #include "assembly.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -15,44 +16,106 @@ struct assembly
   struct entry *lookup_table;
 };
 
-void assembly_set(struct assembly *as, int addr, const char *text)
+#define NOT_FOUND -1
+
+// Hashing with linear probing...
+
+int assembly_find_entry(struct assembly *as, int addr)
 {
-  int k = addr % as->table_size;
-  while (as->lookup_table[k].valid)
+  int idx = addr % as->table_size;
+  for (int offset = 0; offset < 4; offset++)
   {
-    // conflict - rehash
-    int old_size = as->table_size;
-    int new_size = 2 * old_size;
-    struct entry *old_table = as->lookup_table;
+    struct entry *e = &(as->lookup_table[idx]);
+    if (e->valid && e->addr == addr)
+      return idx;
+    ++idx;
+    if (idx == as->table_size)
+      idx = 0;
+  }
+  return NOT_FOUND;
+}
+
+int assembly_find_empty(struct assembly *as, int addr)
+{
+  int idx = addr % as->table_size;
+  for (int offset = 0; offset < 4; offset++)
+  {
+    struct entry *e = &(as->lookup_table[idx]);
+    if (!e->valid)
+      return idx;
+    ++idx;
+    if (idx == as->table_size)
+      idx = 0;
+  }
+  return NOT_FOUND;
+}
+
+void assembly_grow_and_rehash(struct assembly *as)
+{
+  int old_size = as->table_size;
+  struct entry *old_table = as->lookup_table;
+  int new_size = 1 + 2 * old_size;
+  int done;
+  do
+  {
+    done = 1;
     as->lookup_table = calloc(sizeof(struct entry), new_size);
+    // printf("Growing assembly size to %d with table at %p\n", new_size, as->lookup_table);
     as->table_size = new_size;
     for (int i = 0; i < old_size; ++i)
     {
       if (old_table[i].valid)
-        assembly_set(as, old_table[i].addr, old_table[i].text);
+      {
+        int new_idx = assembly_find_empty(as, old_table[i].addr);
+        if (new_idx == NOT_FOUND)
+        {
+          // printf(" ** Collision, need a bigger table **\n");
+          free(as->lookup_table);
+          done = 0;
+          new_size = 1 + new_size * 2;
+          break;
+        }
+        // printf("    %d -> %d\n", i, new_idx);
+        as->lookup_table[new_idx] = old_table[i];
+      }
     }
-    free(old_table);
-    k = addr % as->table_size;
+  } while (!done);
+  // printf("Done rehashing\n");
+  free(old_table);
+}
+
+void assembly_set(struct assembly *as, int addr, const char *text)
+{
+  int idx = assembly_find_entry(as, addr);
+  if (idx == NOT_FOUND)
+  {
+    idx = assembly_find_empty(as, addr);
+    while (idx == NOT_FOUND)
+    {
+      // printf("No free entry for %d\n", addr);
+      assembly_grow_and_rehash(as);
+      idx = assembly_find_empty(as, addr);
+    }
   }
-  struct entry *e = as->lookup_table + k;
-  e->addr = addr;
+  // printf("Setting entry at %d for addr %d\n", idx, addr);
+  struct entry *e = &(as->lookup_table[idx]);
   e->text = strdup(text);
+  e->addr = addr;
   e->valid = 1;
 }
 
 const char *assembly_get(struct assembly *as, int addr)
 {
-  int k = addr % as->table_size;
-  struct entry *e = as->lookup_table + k;
-  if (e->valid && e->addr == addr)
-    return e->text;
-  return NULL;
+  int idx = assembly_find_entry(as, addr);
+  if (idx == NOT_FOUND)
+    return "";
+  return as->lookup_table[idx].text;
 }
 
 struct assembly *assembly_create()
 {
   struct assembly *as = (struct assembly *)malloc(sizeof(struct assembly));
-  int size = 256;
+  int size = 4;
   as->table_size = size;
   struct entry *table = calloc(sizeof(struct entry), size);
   as->lookup_table = table;
